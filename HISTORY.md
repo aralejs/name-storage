9f1f62936cecc47de34adb6bf5d86311b146092b
chencheng <sorrycc@gmail.com>
2014-05-16 10:44:22 +0800

    1.1.0, upgrade to spm@3x

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+# History
+
+---
+
+## 1.1.0
+
+`improved` 升级到 spm@3.x 规范。
--- tests/name-storage-spec.js
+++ tests/name-storage-spec.js
@@ -1,55 +1,51 @@
-define(function(require) {
+// 随机初始窗口名称。
+var random = Math.random();
+var ORIGIN_NAME;
+if(random < 0.33){
+  ORIGIN_NAME = "";
+}else if(random < 0.66){
+  ORIGIN_NAME = "named-window";
+}else{
+  ORIGIN_NAME = "nameStorage:named-storage?x=8&y=9&z=10"
+}
+window.name = ORIGIN_NAME;
 
-  // 随机初始窗口名称。
-  var random = Math.random();
-  var ORIGIN_NAME;
-  if(random < 0.33){
-    ORIGIN_NAME = "";
-  }else if(random < 0.66){
-    ORIGIN_NAME = "named-window";
-  }else{
-    ORIGIN_NAME = "nameStorage:named-storage?x=8&y=9&z=10"
-  }
-  window.name = ORIGIN_NAME;
+var expect = require("expect.js");
 
-  var expect = require("expect");
+describe('nameStorage: '+ORIGIN_NAME, function() {
 
-  describe('nameStorage: '+ORIGIN_NAME, function() {
-
-    it('init', function(done) {
-      require.async('name-storage', function(nameStorage){
-        expect(nameStorage.toString()).to.equal(toString(ORIGIN_NAME));
-        done();
-      });
+  it('init', function(done) {
+    require.async('../index.js', function(nameStorage){
+      expect(nameStorage.toString()).to.equal(toString(ORIGIN_NAME));
+      done();
     });
-
-    it('nameStorage.setItem() and getItem()', function() {
-      nameStorage.setItem("a", 1);
-      expect(nameStorage.getItem("a")).to.equal("1");
-    });
-
-    it('nameStorage.removeItem()', function() {
-      var key = "b";
-      var val = 2;
-      nameStorage.setItem(key, val);
-      nameStorage.removeItem(key);
-      expect(nameStorage.getItem(key)).to.equal(null);
-    });
-
-    it('nameStorage.clear() and toString()', function() {
-      var key = "c";
-      var val = 3;
-      nameStorage.setItem(key, val);
-      nameStorage.clear();
-      expect(nameStorage.getItem(key)).to.equal(null);
-      expect(nameStorage.toString()).to.equal(toString(ORIGIN_NAME).replace(/\?.*/, ""));
-    });
-
   });
 
-  var SCHEME = "nameStorage:";
-  function toString(name){
-    return name.indexOf(SCHEME) === 0 ? name : SCHEME + name;
-  }
+  it('nameStorage.setItem() and getItem()', function() {
+    nameStorage.setItem("a", 1);
+    expect(nameStorage.getItem("a")).to.equal("1");
+  });
+
+  it('nameStorage.removeItem()', function() {
+    var key = "b";
+    var val = 2;
+    nameStorage.setItem(key, val);
+    nameStorage.removeItem(key);
+    expect(nameStorage.getItem(key)).to.equal(null);
+  });
+
+  it('nameStorage.clear() and toString()', function() {
+    var key = "c";
+    var val = 3;
+    nameStorage.setItem(key, val);
+    nameStorage.clear();
+    expect(nameStorage.getItem(key)).to.equal(null);
+    expect(nameStorage.toString()).to.equal(toString(ORIGIN_NAME).replace(/\?.*/, ""));
+  });
 
 });
+
+var SCHEME = "nameStorage:";
+function toString(name){
+  return name.indexOf(SCHEME) === 0 ? name : SCHEME + name;
+}
